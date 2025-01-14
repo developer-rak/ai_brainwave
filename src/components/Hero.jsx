74daddddf53eb0c000d276fa2ac5691e75070e1f
Rashid-Ali-Kalhoro <87558577+Rashid-Ali-Kalhoro@users.noreply.github.com>
2025-01-14 17:35:50 +0500

    company Logos added to hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,6 +8,7 @@ import { ScrollParallax } from "react-just-parallax";
 import { useRef } from "react";
 import Generating from "./Generating";
 import Notification from "./Notification";
+import CompanyLogos from "./CompanyLogos";
 
 const Hero = () => {
    const parallaxRef = useRef(null);
@@ -94,6 +95,8 @@ const Hero = () => {
 
             <BackgroundCircles />
          </div>
+
+         <CompanyLogos className="relative z-10 hidden mt-20 lg:block" />
       </div>
 
       <BottomLine />
