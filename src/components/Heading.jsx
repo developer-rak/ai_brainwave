affdbc8788546fc846932233e8cf73b4aff22d4a
Rashid-Ali-Kalhoro <87558577+Rashid-Ali-Kalhoro@users.noreply.github.com>
2025-01-26 11:43:51 +0500

    added services all section parts including left and right portion

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,9 +1,8 @@
-const Heading = ({ className, title }) => {
+const Heading = ({ className, title, text }) => {
   return (
-    <div className={`${className} max-w-[50rem] mx-auto mb-12 lg:mb-20`}>
-      {title && 
-        <h2 className="h2">{title}</h2>
-      }
+    <div className={`${className} max-w-[50rem] mx-auto mb-12 lg:mb-20 md:text-center`}>
+      {title && <h2 className="h2">{title}</h2>}
+      {text && <p className="mt-4 body-2 text-n-4">{text}</p>}
     </div>
   );
 }
